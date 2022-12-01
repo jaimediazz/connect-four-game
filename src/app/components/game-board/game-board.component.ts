16ca3685a8ae0da90940ad7f6dca87a743613990
Jaime <jaime99dr@gmail.com>
2022-12-01 11:58:25 +0000

    game movements logic done

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -6,8 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-board.component.css']
 })
 export class GameBoardComponent {
+  showDisc: boolean = false;
+  currentPlayer: number = 1;
+  positionState: number[] = 
+    [0,0,0,0,0,0,0,
+      0,0,0,0,0,0,0,
+      0,0,0,0,0,0,0,
+      0,0,0,0,0,0,0,
+      0,0,0,0,0,0,0,
+      0,0,0,0,0,0,0];
 
   positionSelected(pos: number) {
-    console.log("Position selected: " + pos);
+    console.log("Position selected (click): " + pos);
+    let columnSelected: number = pos % 7;       // CALCULATE COLUMN SELECTED
+    let position: number = this.positionState.findIndex(
+      (element, index) => index % 7 === columnSelected && element !== 0); // CALCULATE THE LOWEST OCCUPIED POS IN THE COLUMN
+
+    console.log("DSASFSDFSAFDSF: " + position);
+    if(position === -1) {
+      this.positionState[columnSelected + 35] = this.currentPlayer; // OCCUPIE LOWEST POS IN THE COLUMN
+      this.changePlayerTurn();
+    } else if(position > 6) {
+      this.positionState[position - 7] = this.currentPlayer;  // OCCUPIE ABOVE POSITION
+      this.changePlayerTurn();
+    }
+  }
+  
+  mouseOverPosition(pos: number) {
+    console.log("Position selected (over): " + pos);
+  }
+  
+  mouseOutPosition(pos: number) {
+    console.log("Position selected (out): " + pos);
+  }
+
+  changePlayerTurn() {
+    this.currentPlayer === 1 ? this.currentPlayer = 2 : this.currentPlayer = 1;
   }
 }
